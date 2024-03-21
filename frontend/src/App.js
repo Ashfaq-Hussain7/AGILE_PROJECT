0354625dc042cb944cf34e9f79cb04cef85d7259
ASHFAQ-HUSSAIN7 <ashfaqhms007@gmail.com>
2024-03-21 09:48:28 +0530

    commiting changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-Hello
-
+      <SignUp/>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Browserroute>
-    </Browserroute>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );
-
