def7774a61b3d14c827cdaf12210b316c04553a8
ASHFAQ-HUSSAIN7 <ashfaqhms007@gmail.com>
2024-03-26 09:17:56 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import SignUp from './Components/SignUp/SignUp';
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignUp from './Components/Authentication/SignUp';
+import HomePage from './Components/Home/HomePage';
+import NavBar from './Components/Navigation/NavBar';
 function App() {
   return (
-    <div className="App">
-      <SignUp/>
+    <div>
+      <NavBar /> 
+      <Routes>
+        <Route  path="/" element={<HomePage/>} />
+         <Route path="/home" element={<HomePage/>} />
+         <Route path="/signup" element={<SignUp/>} />
+      </Routes>
     </div>
   );
 }
